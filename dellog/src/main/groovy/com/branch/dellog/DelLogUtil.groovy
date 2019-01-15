ebbac39ab27eb268f5d1cb712f10355b5e84e9be
zhengyongxiang <924106037@qq.com>
2019-01-15 09:33:35 +0800

    Updated:删除imort，另外不要留下一个分号

--- dellog/src/main/groovy/com/branch/dellog/DelLogUtil.groovy
+++ dellog/src/main/groovy/com/branch/dellog/DelLogUtil.groovy
@@ -3,29 +3,30 @@ package com.branch.dellog
 /**
  * Created by Ryze on 2017-2-5.
  */
-public class DelLogUtil {
+class DelLogUtil {
 
-  public static final String head = 'Log.';
-  public static final String tail_end = ');';
-  public static final String suffix = '.java';
-  public static final String charset = "utf-8";
+  public static final String head = 'Log.'
+  public static final String tail_end = ');'
+  public static final String suffix = '.java'
+  public static final String importLog = 'import android.util.Log;'
+  public static final String charset = "utf-8"
 
-  public static void delLog(File rootFile) {
+  static void delLog(File rootFile) {
 
     if (rootFile == null) {
-      return;
+      return
     }
 
     if (rootFile.isDirectory()) {
       rootFile.eachFile {File file ->
         if (file.isFile()) {
           if (file.canRead() && file.name.endsWith(suffix)) {
-            println "file: " + file.getAbsolutePath();
-            delFileLog(file);
+            println "file: " + file.getAbsolutePath()
+            delFileLog(file)
           }
         } else if (file.isDirectory()) {
-          println "dir: " + file.getAbsolutePath();
-          delLog(file);
+          println "dir: " + file.getAbsolutePath()
+          delLog(file)
         }
       }
     }
@@ -33,38 +34,41 @@ public class DelLogUtil {
 
 
   private static void delFileLog(File file) {
-    def endFlag = 0;
-    File ftmp = File.createTempFile(file.getAbsolutePath(), ".tmp");
-    def printWriter = ftmp.newPrintWriter(charset);
-    def reader = file.newReader(charset);
-    def tmpline = null;
-    String line;
+    def endFlag = 0
+    File ftmp = File.createTempFile(file.getAbsolutePath(), ".tmp")
+    def printWriter = ftmp.newPrintWriter(charset)
+    def reader = file.newReader(charset)
+    def tmpline = null
+    String line
     while ((line = reader.readLine()) != null) {
       if (line != null) {
-        tmpline = line.trim();
+        tmpline = line.trim()
         if (tmpline.startsWith(head) || endFlag == 1) {
 
           if (tmpline.endsWith(tail_end)) {
-            endFlag = 0;
-            printWriter.write(";\n")
+            endFlag = 0
+            printWriter.write("\n")
             continue
           } else {
-            endFlag = 1;
+            endFlag = 1
             continue
           }
-        } else {
-          printWriter.write(line + "\n");
+        } else if(tmpline == importLog){
+          // 删除import
+          printWriter.write("\n")
+        }else{
+          printWriter.write(line + "\n")
         }
       }
     }
 
-    reader.close();
+    reader.close()
 
-    printWriter.flush();
-    printWriter.close();
+    printWriter.flush()
+    printWriter.close()
 
-    file.delete();
-    ftmp.renameTo(file.getAbsolutePath());
+    file.delete()
+    ftmp.renameTo(file.getAbsolutePath())
   }
 
 }
